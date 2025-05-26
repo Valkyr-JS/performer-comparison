12a1ee87ae706f20fecd15288f82caa8e61bf7f8
ValkyrJS <valkyr.photos@gmail.com>
2025-05-26 23:37:29 +0100

    Outline

--- src/layouts/OneVsOneBoard/OneVsOneBoard.tsx
+++ src/layouts/OneVsOneBoard/OneVsOneBoard.tsx
@@ -3,6 +3,8 @@ import styles from "./OneVsOneBoard.module.scss";
 
 interface OneVsOneBoardProps {}
 
+const url = import.meta.env.STORYBOOK_STASH_SERVER;
+
 const OneVsOneBoard: React.FC<OneVsOneBoardProps> = () => {
   return (
     <section className={styles["one-vs-one-board"]}>
@@ -11,8 +13,14 @@ const OneVsOneBoard: React.FC<OneVsOneBoardProps> = () => {
         <button>*</button>
       </header>
       <div className={styles["profiles"]}>
-        <Profile />
-        <Profile />
+        <Profile
+          name="Dani Daniels"
+          image={url + "/performer/91/image?t=1746466658"}
+        />
+        <Profile
+          name="Danielle Sharp"
+          image={url + "/performer/1/image?t=1748116759"}
+        />
       </div>
       <div className={styles["tools"]}>
         <button>Undo</button>
@@ -24,6 +32,19 @@ const OneVsOneBoard: React.FC<OneVsOneBoardProps> = () => {
 export default OneVsOneBoard;
 
 /** TEMP */
-const Profile = () => {
-  return <div>Profile</div>;
+
+interface ProfileProps {
+  image: string;
+  name: string;
+}
+
+const Profile = (props: ProfileProps) => {
+  return (
+    <div className={styles["profile"]}>
+      <img src={props.image} alt="" />
+      <div>
+        <button type="button">{props.name}</button>
+      </div>
+    </div>
+  );
 };
