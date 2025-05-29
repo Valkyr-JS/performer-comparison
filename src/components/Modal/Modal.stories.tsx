645ad3f9ef64ef2d38afa84382389d6c3a485e57
ValkyrJS <valkyr.photos@gmail.com>
2025-05-29 17:52:09 +0100

    Bootstrap-based modal

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import Modal from "./Modal";
 
@@ -9,4 +10,11 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const EndGlicko: Story = {};
+export const EndGlicko: Story = {
+  args: {
+    children: <p>Would you like to submit and view the results?</p>,
+    buttons: [],
+    show: true,
+    title: "Tournament complete",
+  },
+};
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,10 +1,49 @@
 import React from "react";
-import styles from "./Modal.module.scss";
+import { default as cx } from "classnames";
 
-interface ModalProps {}
+interface ModalProps extends React.PropsWithChildren {
+  buttons: React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  >[];
+  title: string;
+  show: boolean;
+}
 
 const Modal: React.FC<ModalProps> = (props) => {
-  return <section className={styles.modal}>Modal</section>;
+  const modalClasses = cx("modal", {
+    show: props.show,
+  });
+  const modalStyles = {
+    display: props.show ? "block" : undefined,
+  };
+  return (
+    <section className={modalClasses} style={modalStyles} tabIndex={-1}>
+      <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">{props.title}</h5>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">{props.children}</div>
+          {props.buttons.length ? (
+            <div className="modal-footer">
+              {props.buttons.map((btn) => (
+                <button {...btn} />
+              ))}
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Modal;
